16360bbea35f192759d43d7cc148626fd0f36e90
gost1k <gost1k337@gmail.com>
2023-09-12 18:12:03 +0300

    feat: created user service

---
+++ user_service/pkg/logging/logging.go
@@ -0,0 +1,27 @@
+package logging
+
+import (
+	"github.com/gost1k337/url_shortener/user_service/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger struct {
+	*zap.SugaredLogger
+}
+
+func NewLogger(cfg *config.Config) Logger {
+	zapConfig := zap.NewDevelopmentConfig()
+
+	if cfg.App.Debug {
+		zapConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	if !cfg.App.Debug && cfg.App.LogOutput != "" {
+		zapConfig.OutputPaths = []string{cfg.App.LogOutput}
+	}
+
+	logger, _ := zapConfig.Build()
+	sugar := logger.Sugar()
+	return Logger{sugar}
+}
